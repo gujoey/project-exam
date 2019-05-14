13990cf61c1e1ca695a26aa22893378de1de2d7b
Øyvind Gujord <oyvindgujord@yvinds-MacBook.local>
2019-05-14 17:46:22 +0200

    build

--- src/components/UserSite/Search/SearchComp.tsx
+++ src/components/UserSite/Search/SearchComp.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, FormGroup, Input } from 'reactstrap';
+import { Form, FormGroup, Input, InputGroup, InputGroupAddon } from 'reactstrap';
 
 interface searchCompProps{ heading:string; suggestedSearches:string }
 
@@ -7,27 +7,35 @@ export default class SearchComp extends React.Component<searchCompProps>{
     constructor(props: any){
         super(props);
         this.handleKeyPress = this.handleKeyPress.bind(this);
+        this.handleSearchClick = this.handleSearchClick.bind(this);
     }
 
     handleKeyPress(){
         const app: any = this;
-        
         console.log(app.searchInput.value);
+    }
+
+    handleSearchClick(){
+        console.log("clicked");
     }
     render(){
         let app: any = this;
 
         return(
             <div className="[ search ]">
-                <span className="[ search__bg-image ]"></span>
-                <div className="[ search__container ]">
-                    <h1>{app.props.heading}</h1>
-                    <Form>
-                        <FormGroup onKeyUp={app.handleKeyPress}>
-                            <Input type="text" innerRef={(node) => app.searchInput = node} placeholder="Search among all of our accommodations" />
-                        </FormGroup>
-                    </Form>
-                    <p className="[ search__suggested ]"><strong>Suggested:</strong> {app.props.suggestedSearches}</p>
+                <div className="[ search__bg-image ]">
+                    <div className="[ search__container ]">
+                        <h1>{app.props.heading}</h1>
+                        <Form>
+                            <FormGroup onKeyUp={app.handleKeyPress}>
+                                <InputGroup>
+                                    <Input className="[ search__input ]" type="text" innerRef={(node) => app.searchInput = node} placeholder="Search among all of our accommodations"/>
+                                    <InputGroupAddon onClick={app.handleSearchClick} className="[ search__icon ]" addonType="append"><i className="fas fa-search"></i></InputGroupAddon>
+                                </InputGroup>
+                            </FormGroup>
+                        </Form>
+                        <p className="[ search__suggested ]"><strong>Suggested:</strong> {app.props.suggestedSearches}</p>
+                    </div>
                 </div>
             </div>
         );
