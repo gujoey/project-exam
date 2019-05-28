fbdca0e28dd0d64495361c4f9e7173f64258b2c3
Øyvind Gujord <oyvindgujord@yvinds-MacBook.local>
2019-05-28 15:01:12 +0200

    build

--- src/components/UserSite/ContactComp/ContactComp.tsx
+++ src/components/UserSite/ContactComp/ContactComp.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import { Form, FormGroup, Label } from 'reactstrap';
 
+interface ContactCompProps{
+    handleInputValidation: any;
+    handleSubmit: any;
+    nameErr: boolean;
+    emailErr: boolean;
+    commentErr: boolean;
+}
 
-export default class ContactComp extends React.Component{
+export default class ContactComp extends React.Component<ContactCompProps>{
+    constructor(props:any){
+        super(props);
+
+        this.handleValidation = this.handleValidation.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
     handleValidation(inputRef:string){
         const app: any = this;        
         let inputValue: string = app.refs[inputRef].value;
 
-        if (inputRef==="checkin" || inputRef==="checkout"){
-            let dateInputValues = {"checkin":app.refs.checkin.value, "checkout":app.refs.checkout.value};
-            app.props.handleInputValidation(inputRef, dateInputValues);
-        }else{
-            app.props.handleInputValidation(inputRef, inputValue);
-        }
+        app.props.handleInputValidation(inputRef, inputValue);
     }
 
     handleSubmit(event:any){
@@ -29,13 +37,13 @@ export default class ContactComp extends React.Component{
                 <Form onSubmit={app.handleSubmit} method="POST" action="http://localhost:8888/project-exam/server/contact.json">
                     <FormGroup>
                         <Label htmlFor="clientName">Name <span className="[ contact__input--required ]">*</span></Label>
-                        <input type="text" id="clientName" name="clientName" placeholder="John Doe" /><br/><br/>
+                        <input type="text" id="clientName" onBlur={() => app.handleValidation("name")} name="clientName" placeholder="John Doe" /><br/><br/>
 
                         <Label htmlFor="email">Email <span className="[ contact__input--required ]">*</span></Label>
-                        <input type="email" id="email" name="email" ref="email" placeholder="name@example.com"/><br/><br/>
+                        <input type="email" id="email" onBlur={() => app.handleValidation("email")} name="email" ref="email" placeholder="name@example.com"/><br/><br/>
 
                         <Label htmlFor="comment">Comment <span className="[ contact__input--required ]">*</span></Label>
-                        <textarea id="comment" name="comment" className="[ ]" ref="comment"  placeholder="Enter your comment"/><br/>
+                        <textarea id="comment" onBlur={() => app.handleValidation("comment")} name="comment" className="[ ]" ref="comment"  placeholder="Enter your comment"/><br/>
                         
                         <span>Fields with an asterisk (<span className="[ contact__input--required ]">*</span>) are mandatory</span><br/><br/>
                         <button className="[ contact__button ]">Send inquiry</button>
--- src/pages/ContactPage/ContactPage.tsx
+++ src/pages/ContactPage/ContactPage.tsx
@@ -3,11 +3,80 @@ import NavbarComp from './../../components/UserSite/Navigation/NavbarComp';
 import ContactComp from './../../components/UserSite/ContactComp/ContactComp';
 
 export default class ContactPage extends React.Component{
+    constructor(props:any){
+        super(props);
+
+        this.state = {
+            nameErr: undefined,
+            emailErr: undefined,
+            commentErr: undefined
+        }
+
+        this.validateInput = this.validateInput.bind(this);
+        this.validateSubmit = this.validateSubmit.bind(this);
+    }
+
+    validateInput(inputRef:string, input:any){
+        const app: any = this;
+
+        switch(inputRef){
+            case "name":
+                if (input===""){
+                    app.setState({nameErr:true});
+                    break;
+                }else{
+                    app.setState({nameErr:false});
+                    break;
+                }
+            case "email":
+                let regExEmail:RegExp = /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{1,63}$/;
+                if (!regExEmail.test(input)){
+                    app.setState({emailErr:true});
+                    break;
+                }else{
+                    app.setState({emailErr:false});
+                    break;
+                }
+            case "comment":
+                if (input===""){
+                    app.setState({commentErr:true});
+                    break;
+                }else{
+                    app.setState({commentErr:false});
+                    break;
+                }
+            default:
+                console.error("switch statement recivied an invalid ref argument " + inputRef);
+                break;
+        }
+    }
+
+    validateSubmit(event:any){
+        const app:any = this;
+        if (app.state.nameErr === true || app.state.emailErr === true || app.state.commentErr === true){
+            event.preventDefault();
+        }else if(app.state.nameErr === undefined || app.state.emailErr === undefined || app.state.commentErr === undefined){ //potential bug
+            event.preventDefault();
+            app.setState({
+                nameErr:true,
+                emailErr: true,
+                commentErr: true
+            });
+        }
+    }
+
     render(){
+        const app: any = this;
         return(
             <div>
                 <NavbarComp currentPage={"contact"}/>
-                <ContactComp/>
+                <ContactComp
+                    handleInputValidation={app.validateInput}
+                    handleSubmit = {app.validateSubmit}
+                    nameErr = {app.state.nameErr}
+                    emailErr = {app.state.emailErr}
+                    commentErr = {app.state.commentErr}
+                />
             </div>
         );
     }
