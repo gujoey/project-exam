410bc9bc51c5cf5a1b46ca121fe4b82dfcb68a81
Øyvind Gujord <oyvindgujord@yvinds-MacBook.local>
2019-05-29 13:21:35 +0200

    build

--- src/components/AdminSite/LoginComp/LoginComp.tsx
+++ src/components/AdminSite/LoginComp/LoginComp.tsx
@@ -1,22 +1,49 @@
 import React from 'react';
 import { Form, FormGroup, Label } from 'reactstrap';
 
+interface LoginCompProps{
+    handleSubmit: any;
+    loginErr: boolean;
+}
 
-export default class LoginComp extends React.Component{
+export default class LoginComp extends React.Component<LoginCompProps>{
+    constructor(props:any){
+        super(props);
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event:any){
+        const app: any = this;
+
+        let submission = {
+            event: event,
+            username: app.refs.username.value,
+            password: app.refs.password.value
+        }
+
+        app.props.handleSubmit(submission);
+    }
+
     render(){
-        return(
-            <div className="[ loginComp ]">
-                <div className="[ loginComp__container ]">
-                    <div className="[ login-admin ]">
-                        <Form>
-                            <FormGroup>
-                                <Label htmlFor="username">Name</Label>
-                                <input type="text" id="username" ref="name" placeholder="Username" />
-                                <span></span><br/><br/>
+        const app: any = this;
+        
+        let inputErr: string = app.props.loginErr === true ? "[ login-admin__input--invalid ]" : "";
+        let inputErrText: string = app.props.loginErr === true ? "Username or passsword is incorrect" : "";
 
-                                <Label htmlFor="password">Name</Label>
-                                <input type="password" id="password" ref="password" placeholder="Password" />
-                                <span></span><br/><br/>
+        return(
+            <div className="[ login-comp ]">
+                <div className="[ login-comp__container ]">
+                <h1 className="[ login-comp__heading ]">Please login to access this site</h1><br/>
+                    <div className="[ login-admin ]">
+                        <Form onSubmit={app.handleSubmit}>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <input className={inputErr} type="text" id="username" ref="username" placeholder="Username" /><br/><br/>
+
+                                <Label htmlFor="password">Password</Label>
+                                <input className={inputErr} type="password" id="password" ref="password" placeholder="Password" />
+                                <span className="[ login-admin__error-text ]">{inputErrText}</span><br/><br/>
                                 
                                 <button className="[ login-admin__button ]">Login</button>
                             </FormGroup> 
--- src/pages/AdminSite/LoginPage/LoginPage.tsx
+++ src/pages/AdminSite/LoginPage/LoginPage.tsx
@@ -2,10 +2,46 @@ import React from 'react';
 import LoginComp from './../../../components/AdminSite/LoginComp/LoginComp';
 
 export default class LoginPage extends React.Component{
+    constructor(props: any){
+        super(props);
+
+        this.state = {
+            loginErr: false
+        }
+
+        this.validateSubmission = this.validateSubmission.bind(this);
+    }
+
+    componentDidMount(){
+        localStorage.setItem("username","admin");
+        localStorage.setItem("password", "HelloWorld");
+    }
+
+    validateSubmission(submission:any){
+        const app: any = this;
+
+        submission.event.preventDefault();
+
+        let cridentials = {
+            username: localStorage.getItem("username"),
+            password: localStorage.getItem("password")
+        };
+
+        if (submission.username !== cridentials.username || submission.password !== cridentials.password){
+            app.setState({loginErr:true});
+        }else{
+            app.props.history.push("/admin/dashboard");
+        }
+    }   
+
     render(){
+        const app: any = this;
         return(
             <div>
-                <LoginComp/>
+                <LoginComp
+                    handleSubmit={app.validateSubmission}
+                    loginErr={app.state.loginErr}
+                />
             </div>
         );
     }
