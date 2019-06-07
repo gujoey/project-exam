66434d80f56e32310a31e542f8b956bc250a233f
Øyvind Gujord <oyvindgujord@yvinds-MacBook.local>
2019-06-07 11:45:24 +0200

    build

--- src/components/AdminSite/EstablishmentsComp/NewEstablishmentComp.tsx
+++ src/components/AdminSite/EstablishmentsComp/NewEstablishmentComp.tsx
@@ -1,56 +1,81 @@
 import React from 'react';
 import { Row, Col } from 'reactstrap';
 
-export default class NewEstablishmentComp extends React.Component{
+interface NewEstablishmentCompProps{
+    handleInputValidation: any;
+    estNameErr: boolean;
+}
+
+export default class NewEstablishmentComp extends React.Component<NewEstablishmentCompProps>{
+    constructor(props:any){
+        super(props);
+
+        this.handleValidation = this.handleValidation.bind(this);
+    }
+
+    handleValidation(inputRef:string){
+        const app: any = this;        
+        let inputValue: string = app.refs[inputRef].value;
+        console.log(inputValue);
+
+        app.props.handleInputValidation(inputRef, inputValue);
+    }
     render(){
+        const app: any = this;
+
+        let validate = {
+            estNameErr: app.props.estNameErr === true ? "[ new-establishment__input--error ]" : "",
+            
+        }
+
         return(
             <div>
                 <form>
+                    <label htmlFor="estName">Establishment name <span className="[ new-establishment__input--required ]">*</span></label>
+                    <input className={validate.estNameErr} id="estName" type="text" onBlur={(()=>{app.handleValidation("estName")})} ref="estName" placeholder="Establishment name"/>
+                    <br/><br/>
+
+                    <label htmlFor="email">E-mail adress <span className="[ new-establishment__input--required ]">*</span></label>
+                    <input id="email" type="email" onBlur={(()=>{app.handleValidation("email")})} ref="email" placeholder="E-mail adress"/>
+                    <br/><br/>
                     <Row>
                         <Col md="6">
-                            <label htmlFor="estName">Establishment name</label>
-                            <input id="estName" type="text" placeholder="Establishment name"/>
+                            <label htmlFor="latitude">Latitude <span className="[ new-establishment__input--required ]">*</span></label>
+                            <input id="latitude" type="text" onBlur={(()=>{app.handleValidation("latitude")})} ref="latitude" placeholder="Establishment latitude"/>
                         </Col>
-
                         <Col md="6">
-                            <label htmlFor="estId">Establishment id</label>
-                            <input id="estId" type="text" placeholder="Establishment id"/>
+                            <label htmlFor="longitude">Longitude <span className="[ new-establishment__input--required ]">*</span></label>
+                            <input id="longitude" type="text" onBlur={(()=>{app.handleValidation("longitude")})} ref="longitude" placeholder="Establishment longitude"/>
                         </Col>
                     </Row>
                     <br/>
                     <Row>
-                        <Col md="6">
-                            <label htmlFor="latitude">Latitude</label>
-                            <input id="latitude" type="text" placeholder="Establishment latitude"/>
+                        <Col md="4">
+                            <label htmlFor="maxGuests">Max guests <span className="[ new-establishment__input--required ]">*</span></label>
+                            <input id="maxGuests" type="text" onBlur={(()=>{app.handleValidation("maxGuests")})} ref="maxGuests" placeholder="max guests"/>
                         </Col>
-                        <Col md="6">
-                            <label htmlFor="longitude">Longitude</label>
-                            <input id="longitude" type="text" placeholder="Establishment longitude"/>
+                        <Col md="4">
+                            <label htmlFor="price">Price per night <span className="[ new-establishment__input--required ]">*</span></label>
+                            <input id="price" type="text" onBlur={(()=>{app.handleValidation("price")})} ref="price" placeholder="Price per night"/>
+                        </Col>
+                        <Col md="4">
+                            <label htmlFor="estId">Establishment id <span className="[ new-establishment__input--required ]">*</span></label>
+                            <input id="estId" type="text" onBlur={(()=>{app.handleValidation("estId")})} ref="estId" placeholder="Establishment id"/>
                         </Col>
                     </Row>
                     <br/>
-                    <Row>
-                        <Col md="6">
-                            <label htmlFor="maxGuests">Max guests</label>
-                            <input id="maxGuests" type="text" placeholder="max guests"/>
-                        </Col>
-                        <Col md="6">
-                            <label htmlFor="price">Price per night</label>
-                            <input id="price" type="text" placeholder="Price per night"/>
-                        </Col>
-                    </Row>
 
-                    <label htmlFor="email">E-mail adress</label>
-                    <input id="email" type="email" placeholder="E-mail adress"/>
+                    <label htmlFor="image">Image url <span className="[ new-establishment__input--required ]">*</span></label>
+                    <input id="image" type="text" onBlur={(()=>{app.handleValidation("imageUrl")})} ref="imageUrl" placeholder="Image url"/>
+                    <br/><br/>
 
-                    <label htmlFor="image">Image url</label>
-                    <input id="image" type="text" placeholder="Image url"/>
+                    <label htmlFor="desctiption">Description <span className="[ new-establishment__input--required ]">*</span></label>
+                    <textarea id="description" onBlur={(()=>{app.handleValidation("description")})} ref="description" placeholder="Description of establishment"/>
+                    <br/><br/>
 
-                    <label htmlFor="desctiption">Description</label>
-                    <textarea id="description" placeholder="Description of establishment"/>
-
-                    <label htmlFor="foodService">Establishment offers foodService</label>
-                    <input id="foodService" type="checkbox"/>
+                    <label htmlFor="foodService">Establishment offers food service <span className="[ new-establishment__input--required ]">*</span></label>
+                    <input id="foodService" type="checkbox" ref="foodService"/>
+                    <br/>
 
                     <button type="button">Create establishment</button>
                 </form>
--- src/pages/AdminSite/EstablishmentsPage/NewEstablishmentPage.tsx
+++ src/pages/AdminSite/EstablishmentsPage/NewEstablishmentPage.tsx
@@ -3,13 +3,128 @@ import NavbarComp from './../../../components/AdminSite/NavbarComp/NavbarComp';
 import NewEstablishmentComp from './../../../components/AdminSite/EstablishmentsComp/NewEstablishmentComp';
 
 export default class NewEstablishmentPage extends React.Component{
+    constructor(props:any){
+        super(props);
+
+        this.state = {
+            nameErr: false,
+            emailErr:false,
+            latitudeErr: false,
+            longitudeErr: false,
+            maxGuestsErr: false,
+            priceErr: false,
+            estIdErr: false,
+            imageUrlErr: false,
+            descriptionErr: false
+        }
+
+        this.validateInput = this.validateInput.bind(this);
+    }
+
+    validateInput(inputRef: string, input: any){
+        const app: any = this;
+        switch(inputRef){
+            case "estName":
+                console.log(inputRef);
+                if (input===""){
+                    app.setState({nameErr:true});
+                    break;
+                }else{
+                    app.setState({nameErr:false});
+                    break;
+                }
+            case "email":
+            console.log(inputRef);
+                let regExEmail:RegExp = /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{1,63}$/;
+                if (!regExEmail.test(input)){
+                    app.setState({emailErr:true});
+                    break;
+                }else{
+                    app.setState({emailErr:false});
+                    break;
+                }
+            case "latitude":
+            console.log(inputRef);
+                if (input===""){
+                    app.setState({latitudeErr:true});
+                    break;
+                }else{
+                    app.setState({latitudeErr:false});
+                    break;
+                }
+            case "longitude":
+            console.log(inputRef);
+                if (input===""){
+                    app.setState({longitudeErr:true});
+                    break;
+                }else{
+                    app.setState({longitudeErr:false});
+                    break;
+                }
+            case "maxGuests":
+            console.log(inputRef);
+                if (input===""){
+                    app.setState({maxGuestsErr:true});
+                    break;
+                }else{
+                    app.setState({maxGuestsErr:false});
+                    break;
+                }
+            case "price":
+            console.log(inputRef);
+                if (input===""){
+                    app.setState({priceErr:true});
+                    break;
+                }else{
+                    app.setState({priceErr:false});
+                    break;
+                }
+            case "estId":
+            console.log(inputRef);
+                if (input===""){
+                    app.setState({estIdErr:true});
+                    break;
+                }else{
+                    app.setState({estIdErr:false});
+                    break;
+                }
+            case "imageUrl":
+            console.log(inputRef);
+                if (input===""){
+                    app.setState({imageUrlErr:true});
+                    break;
+                }else{
+                    app.setState({imageUrlErr:false});
+                    break;
+                }
+            case "description":
+            console.log(inputRef);
+                if (input===""){
+                    app.setState({imageUrlErr:true});
+                    break;
+                }else{
+                    app.setState({imageUrlErr:false});
+                    break;
+                }
+                
+            default:
+            console.log(inputRef);
+                console.error(`switch statement "validateInput" recivied an invalid ref argument "${inputRef}"`);
+                break;
+        }
+    }
+
     render(){
+        const app: any = this;
         return(
             <div>
                 <NavbarComp currentPage={"establishments"}/>
                 <div className="[ new-establishment ]">
                     <h1 className="[ new-establishment__heading ]">Create new establishment</h1><br/>
-                    <NewEstablishmentComp/>
+                    <NewEstablishmentComp
+                        handleInputValidation={app.validateInput}
+                        estNameErr={app.state.nameErr}
+                    />
                 </div>
             </div>
         );
