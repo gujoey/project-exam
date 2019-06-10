2abda60e51dd4528d6302771bac850024e5628cf
Øyvind Gujord <oyvindgujord@yvinds-MacBook.local>
2019-06-10 22:43:34 +0200

    build

--- src/components/AdminSite/EstablishmentsComp/NewEstablishmentCreatedComp.tsx
+++ src/components/AdminSite/EstablishmentsComp/NewEstablishmentCreatedComp.tsx
@@ -3,7 +3,13 @@ import React from 'react';
 export default class NewEstablishmentCreatedComp extends React.Component{
     render(){
         return(
-            <div>Hello World</div>
+            <div className="[ establishment-created ]">
+                <h1 className="[ establishment-created__heading ]">New establishment created</h1>
+                <p>
+                    You have succsessfully created a new establishment.
+                    The establishment will be available on the site in a few seconds!
+                </p>
+            </div>
         );
     }
 }
